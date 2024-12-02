2d2c63c0d16e5ebe2c47781d264504dfd4ce076d
Lesley <mutsambiwalesley@gmail.com>
2024-12-02 20:19:40 +0200

    done

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
+let color = "black";
+
 document.addEventListener("DOMContentLoaded",function(){
-    createBoard(16)
-    console.log("hi");
+    createBoard(16);
+
 })
 
  function createBoard(size) {
     let board = document.querySelector(".board");
 
-    board.style.gridTemplateColumns = 'repeat(${size}, 1fr)';
-    board.style.gridTemplateRows = 'repeat(${size}, 1fr)';
+    board.style.gridTemplateColumns =  ' repeat(${size}, 1fr) ';
+    board.style.gridTemplateRows = ' repeat(${size}, 1fr)';
 
     let numDivs = size * size 
 
     for (let i = 0; i < numDivs; i++){
         let div = document.createElement("div");
-        div.style.backgroundColor = "yellow";
-        board.insertAdjacentElement("beforeend", div)
+        div.addEventListener("mouseover", colorDiv);
+        board.insertAdjacentElement("beforeend", div);
     }
+ }
+
+ function getSize(){
+    let input = prompt("Enter the size of the board");
+    let message = document.querySelector("#message");
+    if (input == ""){
+        message.innerHTML = "Please provide a number"
+    }
+    else if (input < 0 || input > 100){
+        message.innerHTML = "Number must be between 1 and 100"
+    }
+    else {
+        message.innerHTML = "Now you can play"
+        return input;
+    }
+ }
+function colorDiv(){
+    if (color == "random"){
+        this.style.backgroundColor = " hsl(${Math.random() * 360}, 100%, 50%) "
+    }
+    else {
+        this.style.backgroundColor = "black";
+    }
+}
+ function setColor(colorChoice) {
+     color = colorChoice;
  }
